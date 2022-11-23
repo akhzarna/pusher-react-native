0b93e3008d9178669d7b5b668ab1974347bb2915
akhzarna <link2akhzar@gmail.com>
2022-11-23 19:46:53 +0500

    Pusher Demo

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import Pusher from 'pusher-js';
+import { useEffect } from 'react';
+
+try {
+  let id = '1590'
+  
+  const pusher = new Pusher('bff5058d89d1b8f2490b', {
+    cluster: 'mt1',
+  });
+  const channel = pusher.subscribe('chat-channel');
+  channel.bind('message-' + id, function (data) {
+    console.log('bind data: ' , data);
+  });
+} catch(error) {
+  console.log(error)
+}
 
 export default function App() {
+  useEffect(() => {
+    Pusher.log = (msg) => {
+      console.log("Annay wa log: " + msg);
+    };
+  },[])  
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Hello</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
